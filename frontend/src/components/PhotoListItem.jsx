60d3659516686d7c7338da72845b2cabb3249882
Michael Gauthier <mikegauthier222@hotmail.com>
2024-02-25 15:47:01 -0800

    render some data through PhotoListItem component on main page-move sample data to App and use props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  return (
+  <div>
+    <img src={props.imageSource} alt="" />
+    <img src={props.profile} alt="" />
+    <h1>{props.city}</h1>
+    <h2>{props.username}</h2>
+  </div>
+  )
 };
 
 export default PhotoListItem;
