c8b69092e18088eb72c5423073cc970691a1506b
Michael Gauthier <mikegauthier222@hotmail.com>
2024-02-27 13:23:16 -0800

    map topiclistitems to topiclist component

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,29 +1,20 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+const TopicList = (props) => {
+  const topics = props.topicListData.map((topic) => (
+    <TopicListItem 
+      title={topic.title}
+      key={topic.id}
+      />
+  ));
 
-const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
       {/* Insert React */}
+      {topics}
     </div>
   );
 };
