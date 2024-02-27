7af8b3925f47ac4f28babdb016bc132ed03d2e06
Michael Gauthier <mikegauthier222@hotmail.com>
2024-02-27 10:13:32 -0800

    build photolist

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,35 +4,18 @@ import { useReducer, useState } from "react";
 import PhotoListItem from "./components/PhotoListItem";
 import "./App.scss";
 import PhotoFavButton from "components/PhotoFavButton";
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+import PhotoList from "components/PhotoList";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const [heart, setHeart] = useState("unlike");
   const switchHeart = () => {
-    setHeart(prevHeart => prevHeart === 'like' ? 'unlike' : 'like')
+    setHeart((prevHeart) => (prevHeart === "like" ? "unlike" : "like"));
   };
 
-  const photos = Array.from({ length: 3 }, (_, index) => (
-    <PhotoListItem
-      heart={heart}
-      switchHeart={switchHeart}
-      photo={sampleDataForPhotoListItem}
-      key={index}
-    />
-  ));
-
-  return <div className="App">{photos}</div>;
+  return <div className="App">
+    <PhotoList />
+    </div>;
 };
 
 export default App;
--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoList.scss";
+import PhotoListItem from "./PhotoListItem";
 
 const sampleDataForPhotoList = [
   {
@@ -56,10 +57,20 @@ const sampleDataForPhotoList = [
   },
 ];
 
-const PhotoList = () => {
+const PhotoList = (props) => {
+
+  const photos = Array.from({ length: 3 }, (_, index) => (
+    <PhotoListItem
+      heart={heart}
+      switchHeart={switchHeart}
+      photo={sampleDataForPhotoList}
+      key={index}
+    />
+  ));
+
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+      {photos}
     </ul>
   );
 };
