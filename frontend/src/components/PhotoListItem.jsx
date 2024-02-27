414f98257c8fe6f9cb18fa956f0cfa47adc42520
Michael Gauthier <mikegauthier222@hotmail.com>
2024-02-27 13:22:32 -0800

    connect proper props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -11,15 +11,15 @@ const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item">
       <PhotoFavButton heart={heart} switchHeart={switchHeart}/>
-      <img className="photo-list__image" src={props.photo.imageSource} alt="" />
+      <img className="photo-list__image" src={props.photo.urls.full} alt="" />
       <div className="photo-list__user-details">
         <img
           className="photo-list__user-profile"
-          src={props.photo.profile}
+          src={props.photo.user.profile}
           alt=""
         />
         <div>
-        <p className="photo-list__user-info">{props.photo.username}</p>
+        <p className="photo-list__user-info">{props.photo.user.name}</p>
         <p className="photo-list__user-location">
           {props.photo.location.city}, {props.photo.location.country}
         </p>
