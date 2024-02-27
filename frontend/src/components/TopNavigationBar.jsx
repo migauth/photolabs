7896849d9e68fe700fde15dfa8547b8596f5b4f6
Michael Gauthier <mikegauthier222@hotmail.com>
2024-02-27 13:47:35 -0800

    add favourites and topic list

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import TopicList from './TopicList';
+import sampleDataForTopicList from "data/sampleData"
+import PhotoFavButton from './PhotoFavButton';
 import '../styles/TopNavigationBar.scss'
+import FavIcon from './FavIcon';
+import FavBadge from './FavBadge';
 
-const TopNavigation = () => {
+
+
+const TopNavigation = (props) => {
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList topicListData={props.topicListData}/>
+      <FavIcon selected />
     </div>
   )
 }
