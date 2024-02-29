52ad1f0de5ffdb529156def4bf3c7fa18f109998
Michael Gauthier <mikegauthier222@hotmail.com>
2024-02-29 10:01:40 -0800

    move onClick event to button istead of img

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,8 +6,8 @@ import closeSymbol from '../assets/closeSymbol.svg';
 const PhotoDetailsModal = ({changeView}) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" onClick={() => changeView('home')}/>
+      <button className="photo-details-modal__close-button" onClick={() => changeView('home')}>
+        <img src={closeSymbol} alt="close symbol"/>
       </button>
     </div>
   )
