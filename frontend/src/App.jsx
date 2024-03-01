7da76db8888bf1a2189e51094378268a853ddfe1
Michael Gauthier <mikegauthier222@hotmail.com>
2024-03-01 14:46:32 -0800

    remove commented out code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,23 +17,6 @@ const App = () => {
     onClosePhotoDetailsModal,
   } = useApplicationData();
   
-  // const toggleFavourite = (id) => {
-  //   if (favourites.includes(id)) {
-  //     // If ID is already in favourites, remove it
-  //     setFavourites(favourites.filter((favId) => favId !== id));
-  //   } else {
-  //     // If ID is not in favourites, add it
-  //     setFavourites([...favourites, id]);
-  //   }
-  // };
-
-  // const [favourites, setFavourites] = useState([]);
-  
-  // // For switching to modal view
-  // const [view, setView] = useState("home");
-
-  // const [photoSelect, setPhotoSelect] = useState(0);
-
   return (
     <div className="App">
       {state.view === "home" && (
