a5389430ea9cace9d442b957bce3054a8bc3f89a
Michael Gauthier <mikegauthier222@hotmail.com>
2024-03-01 14:36:13 -0800

    update custom hooks

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -7,10 +7,20 @@ export default function useApplicationData() {
 
   const [photoSelect, setPhotoSelect] = useState(0);
 
+  const toggleFavourite = (id) => {
+    if (favourites.includes(id)) {
+      // If ID is already in favourites, remove it
+      setFavourites(favourites.filter((favId) => favId !== id));
+    } else {
+      // If ID is not in favourites, add it
+      setFavourites([...favourites, id]);
+    }
+  };
+
   return {
     state,
-    setPhotoSelect,
-    updateToFavPhotoIds,
-    onClosePhotoDetailsModal,
+    setPhotoSelected: setView,
+    updateToFavPhotoIds: toggleFavourite,
+    onClosePhotoDetailsModal: setPhotoSelect,
   };
 }
